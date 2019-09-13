675501a64ea056495ce4a7f71dc2181d3a69a7fa
unknown <C:\Users\AK\AppData\Roaming\The Bat!\Mail>
2019-09-13 17:34:46 +0200

    Added Navigation Menu.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 import {Home} from './components/Home'
 import {Department} from './components/Department'
 import {Employee} from './components/Employee'
-
-import Button from 'react-bootstrap/Button'
+import {Navigation} from './components/Navigation'
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
@@ -14,11 +13,18 @@ function App() {
   return (
     <BrowserRouter>
     <div className="container">
+
+      <h3 className="m-3 d-flex justify-content-center">
+      React JS with Web api Demo</h3>
+      <h5 className="m-3 d-flex justify-content-center">
+      Employee Management Portal</h5>   
+
+      <Navigation/>
+
       <Switch>
         <Route path='/' component={Home} exact />
         <Route path='/department' component={Department}/>
         <Route path='/employee' component={Employee}/>
-        {/* <Route path='/department' component={Department}/>                    */}
       </Switch>
     </div>
     </BrowserRouter>
