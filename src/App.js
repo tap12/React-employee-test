57ee3389b658e0c312b8305fb98df16d97f28dd8
unknown <C:\Users\AK\AppData\Roaming\The Bat!\Mail>
2019-09-13 16:01:30 +0200

    Installed react-bootstrap and created first bootstrap button.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from 'react-bootstrap/Button'
+
 function App() {
   return (
     <div className="App">
-      <h2>Hello World!!</h2>  
+      <h2>Hello World!!</h2>
+
+      <Button variant="primary">Bootsrap btn</Button> 
     </div>
   );
 }
