76a205da96a5269888eb2a2492e3764b59417f2e
unknown <C:\Users\AK\AppData\Roaming\The Bat!\Mail>
2019-09-13 17:03:42 +0200

    Added newly created React components: Home.js, Department.js, Employee.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Home} from './components/Home'
+import {Department} from './components/Department'
+import {Employee} from './components/Employee'
+
 import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello World!!</h2>
-
-      <Button variant="primary">Bootsrap btn</Button> 
+    <div className="container">
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Employee.js
@@ -0,0 +1,13 @@
+
+import React,{Component} from 'react';
+
+export class Employee extends Component {
+
+    render(){
+        return(
+        <div className="mt-5 d-flex jsutify-content-left">
+        <h3>This is the Employee page.</h3>
+        </div> 
+        )   
+    }
+}
